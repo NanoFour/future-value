5f09b194618c4e0de2462db39833a9c28109bc3e
NanoFour <112641851+NanoFour@users.noreply.github.com>
2022-11-01 14:39:28 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+    var futureValue;
+    
+    // get user entries
+    var investment = prompt("Enter investment amount as xxxxx.xx", 10000);
+    investment = parseFloat(investment);
+    var rate = prompt("Enter interest rate as xx.x", 7.5);
+    rate = parseFloat(rate);
+    var years = prompt("Enter number of years", 10);
+    years = parseInt(years);
+    
+    // calulate future value
+    futureValue = investment;
+    for (var i = 1; i <= years; i++ ) {
+      futureValue = futureValue + (futureValue * rate / 100);
+    }
+    futureValue = parseInt(futureValue);
+
+    function pageLoad(){
+      document.write("Investment amount = " + investment);
+      document.write(" Interest rate = " + rate);
+      document.write(" Years = " + years);
+      document.write(" Future Value is " + futureValue + "<br><br>");
+    }
